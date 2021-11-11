62785b6d281a5e8e84bc50303e30d0a1da2d4040
Priyanka-Kasireddy <priyankakasireddy2000@msitprogram.net>
2021-11-12 00:49:49 +0530

    Create a Bar Char

--- hw6_protein.py
+++ hw6_protein.py
@@ -281,6 +281,15 @@ Returns: None
 '''
 def createChart(xLabels, freqList1, label1, freqList2, label2, edgeList=None):
     import matplotlib.pyplot as plt
+    import numpy as np
+    w=0.4
+    xvalues=np.arange(len(xLabels))
+    plt.bar(xvalues,freqList1,width=-w,align='edge',label=label1,edgecolor=edgeList)
+    plt.bar(xvalues,freqList2,width=w,align='edge',label=label2,edgecolor=edgeList)
+    plt.xticks(ticks=list(range(len(xLabels))),labels=xLabels,rotation="horizontal")
+    plt.legend()
+    plt.title("Comparision of Frequencies")
+    plt.show()
     return
 
 
@@ -331,11 +340,11 @@ if __name__ == "__main__":
     # test.testFindAminoAcidDifferences()
 
     ## Uncomment these for Week 3 ##
-    """
+    
     print("\n" + "#"*15 + " WEEK 3 TESTS " +  "#" * 16 + "\n")
     test.week3Tests()
     print("\n" + "#"*15 + " WEEK 3 OUTPUT " + "#" * 15 + "\n")
     runFullProgram()
-    """
+
     # test.testMakeAminoAcidLabels()
-    test.testSetupChartData()
+    # test.testSetupChartData()
